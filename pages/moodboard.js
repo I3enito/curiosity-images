565e91990976de63c787904bb959aa2881f6f83d
Simon <simimueller@hotmail.com>
2020-11-27 10:41:22 +0100

    change moodboard to fullscreen

--- pages/moodboard.js
+++ pages/moodboard.js
@@ -4,7 +4,7 @@ function Moodboard() {
     return (
         <iframe
             style={moodboardStyle}
-            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FLgjTmJCePvPDz1q24JA1Xs%2Fcurious.image%3Fnode-id%3D9%253A2%26viewport%3D428%252C770%252C0.303740531206131%26scaling%3Dmin-zoom"
+            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FLgjTmJCePvPDz1q24JA1Xs%2Fcurious.image%3Fnode-id%3D9%253A19%26viewport%3D701%252C553%252C0.1375698298215866%26scaling%3Dmin-zoom"
             allowfullscreen
         ></iframe>
     );
@@ -12,9 +12,10 @@ function Moodboard() {
 
 const moodboardStyle = {
     border: "1px solid rgba(0, 0, 0, 0.1)",
+    position: "absolute",
     width: "100%",
     margin: "auto",
-    height: "800px",
+    height: "100%",
     display: "flex",
     alignItems: "center",
 };
