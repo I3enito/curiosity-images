3e545580184586427362254bd20693a8cf9e1106
seimon-m <simimueller@hotmail.com>
2020-12-02 14:13:35 +0100

    add video

---
+++ pages/landing-page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <video autoPlay muted loop src="/landing.webm" type="video/webm"></video>
+        </div>
+    );
+}
+
+export default LandingPage;
