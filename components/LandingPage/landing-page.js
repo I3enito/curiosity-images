7d78d7048da2fdddc3f6bb898a23363430fd59b0
seimon-m <simimueller@hotmail.com>
2020-12-02 16:11:52 +0100

    change some css

--- components/LandingPage/landing-page.js
+++ components/LandingPage/landing-page.js
@@ -12,7 +12,7 @@ export const LandingPage = () => {
                 src="/landing.webm"
                 type="video/webm"
             ></video>
-            <motion.div animate={{ x: 800, y: 300, color: "#FF5500", }} transition={{ ease: "easeOut", duration: 4 }}>
+            <motion.div css={styles.motion} initial={{x: -20, y: 300, opacity: 0}} animate={{ x: 800, y: 300, color: "#FF5500", opacity: 1}} transition={{ ease: "easeOut", duration: 4 }}>
                 <h1>Hello</h1>
             </motion.div>
         </div>
--- components/LandingPage/landing-page.styles.js
+++ components/LandingPage/landing-page.styles.js
@@ -3,9 +3,16 @@ import { css } from "@emotion/react";
 export const landing = css`
     video {
         position: fixed;
-        right: 0;
-        bottom: 0;
+        top: 50%;
+        left: 50%;
         min-width: 100%;
         min-height: 100%;
+        width: auto;
+        height: auto;
+        transform: translateX(-50%) translateY(-50%);
     }
+`;
+
+export const motion = css`
+    width: fit-content;
 `;
