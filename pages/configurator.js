f99d1a2457e601552e9b1b8ff8764fac25902646
seimon-m <simimueller@hotmail.com>
2020-12-10 13:41:20 +0100

    add more sub pages

--- pages/configurator.js
+++ pages/configurator.js
@@ -8,9 +8,30 @@ function Configurator() {
     const [page, setPage] = useState("rover");
     const [rover, setRover] = useState("");
     const [cam, setCam] = useState("");
+    const [sol, setSol] = useState(0);
 
+    useEffect(() => {
+        // Always do navigations after the first render
+        router.push("configurator", undefined, { shallow: true });
+    }, []);
+
+    // Rover selection
+    const selectRover = (rover) => {
+        setRover(rover);
+        setPage("cam");
+        const href = `configurator/?rover=${rover}`;
+        router.push(href, href, { shallow: true });
+    };
+
+    // Cam selection
+    const selectCam = (cam) => {
+        setCam(cam);
+        setPage("sol");
+        const href = `configurator/?rover=${rover}&cam=${cam}`;
+        router.push(href, href, { shallow: true });
+    };
     const cameras = [
-        "Front Hazard Avoidance Camera	",
+        "Front Hazard Avoidance Camera",
         "Rear Hazard Avoidance Camera",
         "Mast Camera",
         "Chemistry and Camera Complex",
@@ -20,37 +41,43 @@ function Configurator() {
         "Panoramic Camera",
         "Miniature Thermal Emission Spectrometer (Mini-TES)",
     ];
+    const cameraList = cameras.map((cam) => (
+        <button key={cam} onClick={() => selectCam(cam)}>{cam}</button>
+    ));
 
-    useEffect(() => {
-        // Always do navigations after the first render
-        router.push("configurator/?", undefined, { shallow: true });
-    }, []);
-
-    const selectCam = (rover) => {
-        setRover(rover);
-        setPage("cam");
-        const href = `configurator/?counter=${rover}`;
+    // Sol selection
+    const selectSol = (sol) => {
+        setSol(sol);
+        setPage("end");
+        const href = `configurator/?rover=${rover}&cam=${cam}&sol=${sol}`;
         router.push(href, href, { shallow: true });
     };
 
-    const cameraList = cameras.map((cam) => <li>{cam}</li>);
+    const sols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    const solList = sols.map((sol) => (
+        <button key={sol.toString()} onClick={() => selectSol(sol)}>{sol}</button>
+    ));
 
     return (
         <div css={styles.conf}>
             {page == "rover" && (
                 <>
                     <h2 className="title">Select your Mars Rover</h2>
-                    <button onClick={() => selectCam("curiousity")}>
+                    <button onClick={() => selectRover("curiousity")}>
                         Curiousity
                     </button>
-                    <button onClick={() => selectCam("opportunity")}>
+                    <button onClick={() => selectRover("opportunity")}>
                         Opportunity
                     </button>
-                    <button onClick={() => selectCam("spirit")}>Spirit</button>
+                    <button onClick={() => selectRover("spirit")}>
+                        Spirit
+                    </button>
                 </>
             )}
             {page == "cam" && (
                 <>
+                    <h2 className="title">Select the camera of {rover}</h2>
                     <ul
                         css={css`
                             color: white;
@@ -58,6 +85,38 @@ function Configurator() {
                     >
                         {cameraList}
                     </ul>
+                </>
+            )}
+            {page == "sol" && (
+                <>
+                    <h2 className="title">Select the initial Sol</h2>
+                    <ul
+                        css={css`
+                            color: white;
+                        `}
+                    >
+                        {solList}
+                    </ul>
+                </>
+            )}
+            {page == "end" && (
+                <>
+                    <h2 className="title">Your Choice:</h2>
+                    <div
+                        css={css`
+                            color: white;
+                            margin: 40px auto;
+
+                            p {
+                                margin: 10px auto;
+                                width: fit-content;
+                            }
+                        `}
+                    >
+                        <p>Rover: {rover}</p>
+                        <p>Camera: {cam}</p>
+                        <p>Sol: {sol}</p>
+                    </div>
                 </>
             )}
         </div>
