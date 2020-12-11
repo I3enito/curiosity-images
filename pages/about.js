e8eb629ad41125bc73b46f914c564dede444c3ca
I3enito <bensiegenthaler@gmail.com>
2020-12-11 11:15:36 +0100

    add about page

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About curious.images</h1>
+      <p>
+        curious.images ist ein Studentenprojekt, welches innerhalb von 3 Wochen
+        realisiert wurde. Der Begeisterung für das Weltall und Raumfahrt haben
+        diese intuitive Plattform hervorgebracht. Die Bilder die in den
+        Gallerien verwendet werden sind die offiziellen Aufnahmen der NASA
+        Rover, welche auf dem Mars seit Jahren unschätzbare Forschung betreiben.
+      </p>
+      <p>
+        Technisch wurde diese Webapplikation im Frontend mit Next.js umgesetzt.
+        Das Backend wurde mit einem Node server in Typescript geschrieben.
+      </p>
+    </div>
+  );
+}
+
+export default About;
